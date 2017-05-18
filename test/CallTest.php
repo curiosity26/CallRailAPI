c11a7e2014008b753b1582362154a3cdbc2965ef
curiosity26 <curiosity26@gmail.com>
2017-05-17 21:24:00 -0400

    Commit composer.lock and ensure date time settings are set for testing

--- test/CallTest.php
+++ test/CallTest.php
@@ -1,29 +1,35 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: alexboyce
  * Date: 6/6/15
  * Time: 4:24 PM
  */
+class CallTest extends PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        date_default_timezone_set('America/New_York');
+    }
 
-class CallTest extends PHPUnit_Framework_TestCase {
+    public function testCall()
+    {
+        $api = new \CallRail\CallRail($_ENV['token']);
 
-  public function testCall() {
-    $api = new \CallRail\CallRail($_ENV['token']);
+        $cr = $api->companies();
 
-    $cr = $api->companies();
+        $this->assertTrue($cr->isSuccess());
 
-    $this->assertTrue($cr->isSuccess());
+        $company = $cr->getBody()->current();
 
-    $company = $cr->getBody()->current();
+        $response = $api->calls($company->id);
 
-    $response = $api->calls($company->id);
+        $this->assertTrue($response->isSuccess());
 
-    $this->assertTrue($response->isSuccess());
+        $calls = $response->getBody();
 
-    $calls = $response->getBody();
-
-    $this->assertInstanceOf('CallRail\\Call\\Collection\\CallCollection', $calls);
-    $this->assertNotEmpty($calls);
-  }
+        $this->assertInstanceOf('CallRail\\Call\\Collection\\CallCollection', $calls);
+        $this->assertNotEmpty($calls);
+    }
 }
